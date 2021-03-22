3c319370877151c9bef5d7c5b9b8466cc868587d
Thapus Simon <thapus@gmail.com>
2021-03-22 23:09:23 +0530

    app.js is the entry point for our server server created using core http module form created to add the hoodies when submitted it would be redirected to '/' location the redirection & routing mechanism is made by using standard nodejs mechanism then the entered data(hoody name) is written into a file

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 const http=require('http');
+const fs=require('fs');
+
 
 const server=http.createServer((req,res)=>{
-    console.log("In Event Loop");
+    const url=req.url;
+    const method=req.method;
+    if(url==='/' && method==="GET"){
+        res.setHeader('Content-Type','text/html');
+        const html=`
+        <html>
+        <body>
+        <form action="/add-product" method="POST">
+        <label for="title">Hoody Name</label>
+        <input type="text" name="title" id="title">
+        <button type="submit">Add Hoody</button>
+        </form>
+        </body>
+        </html>
+        
+        `;
+        res.write(html);
+        return res.end();
+        
+    }
+    if(url==="/add-product" && method==="POST"){
+        const body=[];
+        req.on('data',chunks=>{
+            body.push(chunks);
+        });
+        req.on('end',()=>{
+            const parsedBody=Buffer.concat(body).toString().split("=");
+            fs.writeFileSync('hoodies.txt',parsedBody[1]);
+            res.statusCode=302;
+            res.setHeader('Location','/');
+            return res.end();
+        });
+        
+    }
 });
 
 server.listen(3000);
