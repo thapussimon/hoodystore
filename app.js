eaf7477cc681cd9b01a5a6219188044c9d91fff7
Thapus Simon <thapus95@gmail.com>
2021-03-23 22:01:15 +0530

    error template finished

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path=require('path');
 const rootDir=require('./utils/path');
 
 
+
 //Creating the express app
 const app=express();
 //By default our express app does not parse data
@@ -16,13 +17,17 @@ const bodyParser=require('body-parser');
 //We need need to setup a middleware which parses the data from the form
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.set('view engine','pug');
+app.set('views',path.join(rootDir,'views','templates'));
+
 app.use(express.static(path.join(rootDir,'public')));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
 
 app.use('/',(req,res,next)=>{
-    res.status(402).sendFile(path.join(rootDir,'views','404.html'));
+    // res.status(402).sendFile(path.join(rootDir,'views','404.html'));
+    res.status(404).render('error');
 });
 
 
