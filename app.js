c3a8645d6ab4ae9c1999a72562fc2e9e611eff9d
Thapus Simon <thapus@gmail.com>
2021-03-23 11:29:50 +0530

    404 error route setup in app.js

--- app.js
+++ app.js
@@ -17,5 +17,9 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use('/',(req,res,next)=>{
+    res.status(402).send('<h1>PAGE NOT FOUND</h1>');
+});
+
 
 app.listen(3000);
