acde7d1a631d9a8f45bf369d0857d406e0051278
Thapus Simon <thapus@gmail.com>
2021-03-23 10:34:47 +0530

    body parser created, for the form in shop page

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ const express=require('express');
 
 //Creating the express app
 const app=express();
+//By default our express app does not parse data
+//To parse data we use another module called body-parser
+//This body-parser cannot parse all kinds of data, its ok for form data
+const bodyParser=require('body-parser');
+//We need need to setup a middleware which parses the data from the form
+app.use(bodyParser.urlencoded({extended:false}));
 
+//This is our middleware for the Product Page
+//This middleware handles the add-products route
+//This is where the admin of the site adds new products
 app.use('/add-product',(req,res,next)=>{
     console.log("In Add Product Page");
+    console.log(req.body.title);
     const html=`
         <html>
         <head>
@@ -21,6 +31,8 @@ app.use('/add-product',(req,res,next)=>{
     res.send(html);
 });
 
+//This is our homepage
+//This middleware handles the homepage route
 app.use('/',(req,res,next)=>{
     console.log("In Shop Page");
     const html=`
