efc15ce5ddd8087ca47c7950168871e5d364ed3c
Thapus Simon <thapus@gmail.com>
2021-03-23 10:01:16 +0530

    created our express app for testing purpose

--- app.js
+++ app.js
@@ -1,42 +1,12 @@
 const http=require('http');
-const fs=require('fs');
+const express=require('express');
 
 
-const server=http.createServer((req,res)=>{
-    const url=req.url;
-    const method=req.method;
-    if(url==='/' && method==="GET"){
-        res.setHeader('Content-Type','text/html');
-        const html=`
-        <html>
-        <body>
-        <form action="/add-product" method="POST">
-        <label for="title">Hoody Name</label>
-        <input type="text" name="title" id="title">
-        <button type="submit">Add Hoody</button>
-        </form>
-        </body>
-        </html>
-        
-        `;
-        res.write(html);
-        return res.end();
-        
-    }
-    if(url==="/add-product" && method==="POST"){
-        const body=[];
-        req.on('data',chunks=>{
-            body.push(chunks);
-        });
-        req.on('end',()=>{
-            const parsedBody=Buffer.concat(body).toString().split("=");
-            fs.writeFileSync('hoodies.txt',parsedBody[1]);
-            res.statusCode=302;
-            res.setHeader('Location','/');
-            return res.end();
-        });
-        
-    }
+//Creating the express app
+const app=express();
+
+app.use((req,res,next)=>{
+    console.log("In middleware");
 });
 
-server.listen(3000);
+app.listen(3000);
